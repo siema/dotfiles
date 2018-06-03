4c9e9cdafe4493bddd5943d4bdc0e5ec12f71db3
Siema <garaddon@gmail.com>
2018-06-03 02:45:58 +0200

    add windows setup script

---
+++ setup.ps1
@@ -0,0 +1,3 @@
+﻿Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install -y 7zip adobereader-update cdburnerxp dropbox foobar2000 gimp googlechrome inkscape irfanview irfanviewplugins jdk8 libreoffice-fresh mpv paint.net sublimetext3
+choco install -y --ignore-checksums steam
